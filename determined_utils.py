fe023e5df5cac861f9672da6b2a74b2bd3f4ca79
Gene Lewis <glewis17@cs.stanford.edu>
2022-10-12 17:21:53 -0400

    Add determined training utility, fix deepcopy bug

--- determined_utils.py
+++ determined_utils.py
@@ -1,6 +1,10 @@
+import copy
+import os
+
+import torch
 from determined.pytorch import DataLoader, PyTorchTrial
 
-from sairg_utils import get_model
+import sairg_utils
 
 class ModelTrial(PyTorchTrial):
   def __init__(self, context):
@@ -17,20 +21,50 @@ class ModelTrial(PyTorchTrial):
                           'checkpoint_prefix',
                          ]
     training_args = {k: self.context.get_hparam(k) for k in training_arg_keys}
+    training_args = copy.deepcopy(training_args)
+    training_args = sairg_utils.import_arg_classes(training_args)
 
-    print(training_args)
-    self.model = get_model(training_args)
-    print(self.model)
-    self.model = self.context.wrap_model(self.model)
+    model = sairg_utils.get_model(training_args)
+    self.model = self.context.wrap_model(model)
+
+    self.loss_fn = training_args['loss_fn_class']()
+    optimizer_key_params = training_args['optimizer_params']
+    optimizer_class = training_args['optimizer_class']
+    optimizer = optimizer_class(self.model.parameters(), **optimizer_key_params)
+    self.optimizer = self.context.wrap_optimizer(optimizer)
+
+    self.dataset = training_args['dataset']
+    self.transforms = training_args['transforms']
 
   def build_training_data_loader(self):
-    pass
+    train_set = self.dataset(root='/data', train=True, download=False, transform=self.transforms)
+    return DataLoader(train_set, batch_size=self.context.get_per_slot_batch_size())
 
   def build_validation_data_loader(self):
-    pass
+    val_set = self.dataset(root='/data', train=False, download=False, transform=self.transforms)
+    return DataLoader(val_set, batch_size=self.context.get_per_slot_batch_size())
 
   def train_batch(self, batch, epoch_idx, batch_idx):
-    pass
+    data, labels = batch
+
+    self.model.train()
+    output = self.model(data)
+    training_loss = self.loss_fn(output, labels)
+
+    self.context.backward(training_loss)
+    self.context.step_optimizer(self.optimizer)
+
+    return {"training_loss": training_loss}
 
   def evaluate_batch(self, batch):
-    pass
+    data, labels = batch
+
+    self.model.eval()
+    with torch.no_grad():
+      output = self.model(data)
+      val_loss = self.loss_fn(output, labels)
+
+      preds = output.argmax(dim=1, keepdim=True)
+      accuracy = preds.eq(labels.view_as(preds)).sum().item() / len(data)
+
+    return {"validation_loss": val_loss, "accuracy": accuracy}
